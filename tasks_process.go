a38f5e733e2541a5fc204ac923ee410713e3c4b3
Andrew Darqui <andrew.darqui@gmail.com>
2013-12-13 19:56:50 -0500

    added pgrep and args to tasks_process.go

--- tasks_process.go
+++ tasks_process.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+//"log"
 	"fmt"
+	"os/exec"
+	"strings"
 )
 
 func Task_Process(M *Main, S *State, TD *Task_Data) (bool) {
@@ -12,6 +15,10 @@ func Task_Process(M *Main, S *State, TD *Task_Data) (bool) {
 		case "running" : return Task_Process_Running(M, S, TD)
 		case "!running" : return Task_Process_Not_Running(M, S, TD)
 		case "!running_single": return Task_Process_Not_Running_Single(M, S, TD)
+		case "!running_args" : return Task_Process_Not_Running_Args(M, S, TD)
+		case "running_args" : return Task_Process_Running_Args(M, S, TD)
+		case "!running_pgrep" : return Task_Process_Not_Running_Pgrep(M, S, TD)
+		case "running_pgrep" : return Task_Process_Running_Pgrep(M, S, TD)
 		default : return false
 	}
 }
@@ -50,6 +57,10 @@ func Task_Process_Running(M *Main, S * State, TD *Task_Data) (bool) {
 }
 
 
+
+
+
+
 func Task_Process_Not_Running(M * Main, S *State, TD *Task_Data) (bool) {
 	return Task_Process_Not_Running_Generic(M, S, TD, false)
 }
@@ -74,6 +85,7 @@ func Task_Process_Not_Running_Generic(M *Main, S *State, TD *Task_Data, Single b
 				break
 			}
 		}
+
 		if found == false {
 			mon := S.MON_Gen_Task(alert_level, check_process, TD.Policy, fmt.Sprintf("%s is not running", check_process), "None")
 			M.M<-mon
@@ -86,14 +98,6 @@ func Task_Process_Not_Running_Generic(M *Main, S *State, TD *Task_Data, Single b
 			/*
 			 * Here's where it gets tricky... We need to notify users of a cleared alert
 			 */
-
-			 /*
-			truth := S.STATE_Hash_Exists(TD.Policy.Name, TD.Policy.Idx, check_process)
-			if truth == true {
-				// This means we've had an alert, that is now clear, so notify
-				S.STATE_Hash_Clear(TD.Policy.Name, TD.Policy.Idx, check_process, TD.Policy)
-			}
-			*/
 			S.STATE_Hash_All_Clear(TD, check_process)
 
 		}
@@ -101,6 +105,109 @@ func Task_Process_Not_Running_Generic(M *Main, S *State, TD *Task_Data, Single b
 
 	return found
 }
+
+
+func Task_Process_Args_Generic(M *Main, S *State, TD *Task_Data, Running bool) (bool) {
+	truth := false
+
+	alert_level := Task_Get_Alert_Level(TD.Policy)
+	/* process:count */
+	Map := make(map[string]int)
+
+	ps := exec.Command("ps", "-e", "-o", "args")
+	output, _ := ps.Output()
+
+	for k, v := range strings.Split(string(output), "\n") {
+		if k == 0 || len(v) == 0 {
+			continue
+		}
+
+		if _,ok := Map[v]; ok {
+			Map[v] += 1
+		} else {
+			Map[v] = 1
+		}
+	}
+
+	for _, param := range TD.Policy.Params {
+		if val, ok := Map[param]; ok {
+
+			if Running == true {
+				/* process found and running true means, notify when this process is running */
+				mon := S.MON_Gen_Task(alert_level, param, TD.Policy, fmt.Sprintf("%s is running (count=%d)", param, val), "None")
+				M.M<-mon
+				truth = true
+			} else {
+				S.STATE_Hash_All_Clear(TD, param)
+			}
+		} else {
+			if Running == false {
+				/* process not found, and running false means, notify when this process is not running */
+				mon := S.MON_Gen_Task(alert_level, param, TD.Policy, fmt.Sprintf("%s is not running", param), "None")
+				M.M<-mon
+				truth = true
+			} else {
+				S.STATE_Hash_All_Clear(TD, param)
+			}
+		}
+	}
+
+	return truth
+}
+
+func Task_Process_Running_Args(M *Main, S *State, TD *Task_Data) (bool) {
+	return Task_Process_Args_Generic(M,S,TD,true)
+}
+
+func Task_Process_Not_Running_Args(M *Main, S *State, TD *Task_Data) (bool) {
+	return Task_Process_Args_Generic(M,S,TD,false)
+}
+
+
+
+func Task_Process_Pgrep_Generic(M *Main, S *State, TD *Task_Data, Running bool) (bool) {
+	truth := false
+
+	alert_level := Task_Get_Alert_Level(TD.Policy)
+	/* process:count */
+	for _, param := range TD.Policy.Params {
+
+		ps := exec.Command("pgrep", "-f", param)
+		output, _ := ps.Output()
+
+		if len(output) > 0 {
+			if Running == true {
+				/* processes found and running = true, bad */
+				mon := S.MON_Gen_Task(alert_level, param, TD.Policy, fmt.Sprintf("%s is running", param), "None")
+				M.M<-mon
+				truth = true
+			} else {
+				S.STATE_Hash_All_Clear(TD, param)
+			}
+		} else {
+			if Running == false {
+				/* process not found and running = false, bad */
+				mon := S.MON_Gen_Task(alert_level, param, TD.Policy, fmt.Sprintf("%s is running", param), "None")
+				M.M<-mon
+				truth = true
+			} else {
+				S.STATE_Hash_All_Clear(TD, param)
+			}
+		}
+
+	}
+
+	return truth
+}
+
+func Task_Process_Running_Pgrep(M *Main, S *State, TD *Task_Data) (bool) {
+	return Task_Process_Pgrep_Generic(M,S,TD,true)
+}
+
+func Task_Process_Not_Running_Pgrep(M *Main, S *State, TD *Task_Data) (bool) {
+	return Task_Process_Pgrep_Generic(M,S,TD,false)
+}
+
 
 
 
